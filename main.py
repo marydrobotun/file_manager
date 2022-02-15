36bf3f744ca7e5dca3ed619662290460430ba2ef
Mary <marydrobotun@gmail.com>
2022-02-15 21:57:17 +0300

    created tables for database

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import sqlite3 as sq
 with sq.connect("file_manager.db") as con:
     cur = con.cursor()
-    cur.execute('')
+    cur.execute("""CREATE TABLE IF NOT EXISTS users (
+    name TEXT,
+    surname TEXT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT)""")
 
+    cur.execute("""CREATE TABLE IF NOT EXISTS favourite_paths (
+    path TEXT,
+    added DATE,
+    id INTEGER PRIMARY KEY AUTOINCREMENT)""")
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS users_to_path (
+    user_id INTEGER,
+    path_id INTEGER,
+    FOREIGN KEY (user_id)  REFERENCES users(id),
+    FOREIGN KEY (path_id)  REFERENCES favourite_paths(id))""")
